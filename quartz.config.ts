2de2bcba48d5f26b038257bd84964d5c2a1bf13d
Hua-Ming Huang <huaming.huang.tw@gmai.com>
2024-07-03 13:59:29 +0200

    Change site name

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Hua-Ming's Digital Garden",
+    pageTitle: "⚛ Enoughness",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
