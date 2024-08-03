42e72a3ff91f91288d943ceef49091093f13effc
Hua-Ming Huang <huaming.huang.tw@gmai.com>
2024-08-03 09:22:20 +0200

    Updating <enoughness> from my laptop (2024-08-03 09:22:18)
    Affected file(s) (only up to 20 files are shown):
    quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -72,7 +72,10 @@ const config: QuartzConfig = {
       Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
       Plugin.GitHubFlavoredMarkdown(),
       Plugin.TableOfContents(),
-      Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
+      Plugin.CrawlLinks({
+        markdownLinkResolution: "absolute",
+        lazyLoad: true
+      }),
       Plugin.Description(),
       Plugin.Latex({ renderEngine: "katex" }),
     ],
